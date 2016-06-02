3a2422304b847318f70a9d59ef551ba57b03db3d
jimfang <jimway21@gmail.com>
2016-06-03 01:56:25 +1000

    Add files via upload
    fix timing too long >5s . fix corner cases

--- codility/lessons/1-MaxBinaryGap.cpp
+++ codility/lessons/1-MaxBinaryGap.cpp
@@ -4,65 +4,43 @@
 
 using namespace std;
 
-// lib support
-int lib_ShiftToNextBit1(int &changeN)
-{
-    int count = 0;
-    do
-    {
-        changeN /= 2;
-        count++;
-    }while(changeN%2 ==0);
-
-    return count;
-}
-
 // algorithm main
 int solution_MaxBinaryGap(int N)
 {
-    int referGap = 0;
-    int referFactor = 0;
+    int maxGap = 0;
 
-    // 1. input check
-    if(abs(N) < 2)
-        return referGap;
+    int cntOne = 0;
+    int gap = 0;
 
-    // 2. initialize reference factors
-    referGap = 1;
-    referFactor = 2;
-
-    // 3. do search with variable reference
-    int currentN = N;
-    while(currentN > referFactor)
+    while(N != 0)
     {
-        int count = 0;
-
-        // trace each iterator
-        cout<<"----- refer: gap=" << referGap << \
-         ". currentN = " << currentN << \
-         ". factor="<<referFactor<<endl;
-
-
-        // condition there is 1 in referfactor
-        if(currentN % referFactor)
+        // check every bit
+        if(N&0x01)
         {
-            // shift currentN to cut nearest 1
-            count = lib_ShiftToNextBit1(currentN);
-            assert(count <= referGap);
+            // tailing 0
+            if(cntOne == 0)
+                gap = 0;
+
+            cntOne++;
         }
         else
         {
-            currentN /= referFactor;
-            count = lib_ShiftToNextBit1(currentN);
-
-            referGap += count;
-            referFactor <<= count;
+            gap++;
         }
 
+        // check ones
+        if(cntOne == 2)
+        {
+            maxGap = max(gap, maxGap);
+            gap = 0;
+            cntOne = 1;
+        }
+
+        N >>= 1;
     }
 
     // 4. return if iterator finished
-    return referGap;
+    return maxGap;
 }
 
 int main()
@@ -70,14 +48,8 @@ int main()
     cout << "Hello world!" << endl;
     int N = 2;
     int gap = 0;
-    gap = solution_MaxBinaryGap(N);
-    cout<< "Number " << N <<": BinaryGap = " << gap << endl;
 
-    N = 8;
-    gap = solution_MaxBinaryGap(N);
-    cout<< "Number " << N <<": BinaryGap = " << gap << endl;
-
-    N = 100000;
+    N = 561892; //42;
     gap = solution_MaxBinaryGap(N);
     cout<< "Number " << N <<": BinaryGap = " << gap << endl;
 
